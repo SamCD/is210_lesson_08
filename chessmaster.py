cc75857a7e24d27f33746ddd87e509d71fc58a01
Sam Cohen-DeVries <samuel.cohen-devries@spsmail.cuny.edu>
2014-10-20 08:51:55 -0400

    Task 06/78

--- chessmaster.py
+++ chessmaster.py
@@ -39,3 +39,34 @@ class ChessPiece(object):
             statement = self.moves
         else:statement = False
         return move
+
+class Rook(ChessPiece):
+
+    def __init__(self, position):
+        self.prefix = "R"
+
+    def is_legal_move(self, position):
+        if self.move(position)[1].split()[1] != self.move(position)[0].split()[1]\
+        and self.move(position)[1].split()[0] != self.move(position)[0].split()[0]:
+            legal = False
+        else:
+            legal = True
+
+class ChessMatch(object):
+
+    def __init__(pieces=None):
+        if pieces is None:
+            self.reset()
+        else:
+            self.pieces = pieces
+            log = []
+
+
+    def reset(self):
+        pieces = {'Ra1': Rook('a1'), 'Rh1': Rook('h1'), 'Ra8': Rook('a8'),
+                  'Rh8': Rook('h8'), 'Bc1': Bishop('c1'), 'Bf1': Bishop('f1'),
+                  'Bc8': Bishop('c8'), 'Bf8': Bishop('f8'), 'Ke1': King('e1'),
+                  'Ke8': King('e8')}
+
+    def move(self, piece, destination):
+        
